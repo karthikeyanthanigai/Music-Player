f893cf349f9a10a07d95038f2b39b548b356f7e5
kathikeyanthanigai <51513480+kathikeyanthanigai@users.noreply.github.com>
2019-12-19 15:52:28 +0530

    Update Source_code.py

--- Source_code.py
+++ Source_code.py
@@ -1,5 +1,5 @@
-from  tkinter import *
-import  tkinter.messagebox
+from tkinter import *
+import tkinter.messagebox
 from tkinter import filedialog
 import os
 import pygame
@@ -18,7 +18,7 @@ submenu = Menu(menubar , tearoff=0)
 
 
 menubar.add_cascade(label = "File",menu = submenu)
-#submenu.add_command(label = "Open",command = browse_file)
+
 submenu.add_command(label = "Exit",command = root.destroy)
 
 def about_us():
@@ -43,17 +43,15 @@ text.pack(pady=20)
 
 
 
-#def stop_music():
- #   mixer.music.stop()
-  #  statusbar['text'] = "music stopped "
 
-def  pause_music():
+
+def pause_music():
     global paused
     paused = True
     mixer.music.pause()
     statusbar['text'] = "music paused"
 
-def  play_music():
+def play_music():
 
         try:
             paused
@@ -65,7 +63,7 @@ def  play_music():
         else:
             mixer.music.unpause()
             statusbar['text'] = "music resumes!!"
-def  ani_music():
+def ani_music():
     c = queue()
     c.enq('01 - uturn.mp3')
 
@@ -79,7 +77,7 @@ def  ani_music():
 
     c.call()
 
-def  sid_music():
+def sid_music():
     s = queue()
     s.enq('01 - emai.mp3')
 
@@ -87,27 +85,30 @@ def  sid_music():
 
     s.enq('03 - Kannaana-Kanney.mp3')
 
-    s.enq('04 - Kurumba.mp3')
+    s.enq('04 - Thalli.mp3')
 
     s.enq('05 - Maate Vinadhuga.mp3')
 
     s.call()
 
-def  hip_music():
+
+
+def hip_music():
     h = queue()
     h.enq('01 - happynewyear.mp3')
 
-    h.enq('02 - single pasanga.mp3')
+    h.enq('02 - aathadi.mp3')
 
     h.enq('03 - meesaya murukku.mp3')
 
-    h.enq('04 - Erangi Vandhu.mp3')
+    h.enq('04 - neeyum.mp3')
 
     h.enq('05 - azhage.mp3')
 
     h.call()
 
-def  next_music():
+
+def next_music():
     n = queue()
 
     n.next()
@@ -115,21 +116,26 @@ def  next_music():
 
 
 
-def  set_vol(val):
+
+def set_vol(val):
     volume=int(val) /100
     mixer.music.set_volume(volume)
 
 
-def  rewind_music():
+def rewind_music():
     pygame.mixer.music.rewind()
     statusbar['text'] = "music rewind!!"
 
-class  queue:
+def replay_music():
+    pygame.mixer.music.play(-1)
+    statusbar['text'] = "music replay!!"
+
+class queue:
     def __init__(self):
         self.queue=[]
         self.limit=5
         self.x=0
-    def  enq(self,data):
+    def enq(self,data):
         if (len(self.queue)==self.limit):
             print("queue overflow")
         else:
@@ -140,29 +146,22 @@ class  queue:
         print(self.queue)
 
 
-     def  music():
+        def music():
             pygame.init()
             pygame.mixer.init()
             pygame.mixer.music.load(self.queue[self.x])
             pygame.mixer.music.play(0)
-            #text = Label(root, text=self.queue[0])
-            #text.pack(pady=20)
             statusbar['text'] = "playing music" + ' - - ' + self.queue[0]
             print('1')
             que()
 
-      def  que():
-
-            #global self.x
-
+        def que():
             pos = pygame.mixer.music.get_pos()
             if int(pos) == -1:
                 self.x = self.x+1
                 pygame.mixer.music.load(self.queue[self.x])
                 pygame.mixer.music.play(0)
                 print('2')
-                #text = Label(root, text=self.queue[self.x])
-                #text.pack(pady=20)
                 statusbar['text'] = "playing music" + ' - - ' + self.queue[self.x]
 
             root.after(1, que)
@@ -170,16 +169,15 @@ class  queue:
         music()
 
 
-    def  deq(self):
+    def deq(self):
         if (len(self.queue)==0):
             print("queue underflow")
         else:
-              self.queue.pop(0)
-    
+
+            self.queue.pop(0)
     def next(self):
-          pygame.mixer.music.stop()
-        
-     def que():
+        pygame.mixer.music.stop()
+        def que():
 
             pos = pygame.mixer.music.get_pos()
             if int(pos) == -1:
@@ -196,6 +194,11 @@ class  queue:
         que()
 
 
+    #def display(self):
+        #for i in range(0,5):
+            #texta = Label (root,text = self.queue[i])
+            #texta.pack(side=TOP)
+            #print(self.queue[i])
 
 
 
@@ -208,22 +211,16 @@ middleframe = Frame(root)
 middleframe.pack(padx=20,pady=20)
 
 
-aniphoto = PhotoImage(file='_anir.png' )
+aniphoto = PhotoImage(file='anirudh.png' )
 anibtn = Button(middleframe, image=aniphoto, command=ani_music)
-#texta = Label (root,text =  'Anirudh')
-#texta.pack(side=LEFT,padx=50,pady=50)
 anibtn.pack(side=LEFT,padx=20)
 
-sidphoto = PhotoImage(file='_sid.png' )
+sidphoto = PhotoImage(file='sidd.png' )
 sidbtn = Button(middleframe, image=sidphoto, command=sid_music)
-#texts = Label (root,text =  'Sid')
-#texts.pack(side=LEFT,padx=50,pady=100)
 sidbtn.pack(side=LEFT,padx=20)
 
-hipphoto = PhotoImage(file='sz_hip.png' )
+hipphoto = PhotoImage(file='hip.png' )
 hipbtn = Button(middleframe, image=hipphoto, command=hip_music)
-#texth = Label (root,text =  'hiphop')
-#texth.pack(side=LEFT,padx=50,pady=100)
 hipbtn.pack(side=LEFT,padx=20)
 
 
@@ -245,10 +242,14 @@ rewindphoto = PhotoImage(file='rewind.png')
 rewindbtn = Button(middleframe, image=rewindphoto,command = rewind_music)
 rewindbtn.pack(side= RIGHT,padx=20)
 
+replayphoto = PhotoImage(file='replay.png')
+replaybtn = Button(middleframe, image=replayphoto,command = replay_music)
+replaybtn.pack(side= RIGHT,padx=20)
+
 scale = Scale(root,from_=0,to=100, orient =HORIZONTAL , command= set_vol)
 scale.set(75)
 mixer.music.set_volume(0.7)
-scale.pack(pady=25)
+scale.pack(pady=20)
 
 statusbar = Label(root,text = "Welcome to Ganna ..",relief = SUNKEN, anchor =W)
 statusbar.pack(side = BOTTOM, fill = X)
